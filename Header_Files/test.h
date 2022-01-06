bdf80aa992188c5cd78928eb752036e5f0e5ddff
rfloyd01 <lynrdskynr05@gmail.com>
2022-01-05 23:39:53 -0500

    made arrayOfArray struct for cuz Matt in test.h

--- Header_Files/test.h
+++ Header_Files/test.h
@@ -1,65 +1,82 @@
 #pragma once
 
 #include <Header_Files/pch.h>
-#include <Header_Files/print.h>
-#include <Header_Files/functions.h> //includes powers_of_two[]
-#include <vector>
-#include <algorithm>
 
-//Non-abundant sums
+//Making a struct to handle an array of arrays, where the struct can hold any number of arrays, and each of these
+//arrays in turn can hold a different amount of elements than the other arrays
+
+struct arrayOfArrays
+{
+	int numberOfArrays;
+	int* arrayLengths;
+	int** overallArray;
+};
+
 std::pair<std::string, double> test()
 {
+	//ignore these lines, I use them in other header files
 	auto run_time = std::chrono::steady_clock::now();
-	long long answer = 1; //answer could be initialized to no-pivot as there's only one case of that
+	long long answer = 0;
 
-	long long goal = 100;
+	//Let's say we want to make the multi-array { {1, 99}, {1, 2, 3, 4}, {1, 39, 50}}. It contains three arrays of length
+    //2, 4 and 3 respectively. This can't be created using standard multi-array [] notation as this only creates things
+    //of uniform size, i.e. int yeet[3][2][2] = {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}, {{9, 10}, {11, 12}}}. To accomplish the 2, 4, 3 array
+    //you would need an array of pointers, where each pointer points to an array of integers (which is also represented by a pointer,
+    //I think you were barking up this tree earlier). The big caveat here though is that when using pointers, the code
+    //has no way to tell how many elements are included in the array that the pointer is pointing to. This is why another
+    //array would need to be kept which keeps track of the individual array lengths, here's a sample struct implementing
+    //how this might work.
+	
+	//first you can define the original arrays
+	int a1_1[2] = { 1, 99 };
+	int a1_2[4] = { 1, 2, 3, 4 };
+	int a1_3[3] = { 1, 39, 50 };
 
-	//maximal partition length found using triangle number equation and quadratic formula
-	long long max_length = (-3 + sqrt(9 + 8 * (goal - 1))) / 2.0, final_value, current_num = 0;
+	//then create another array with pointers to these individual arrays
+	int* combined_a1[3] = { a1_1, a1_2, a1_3 };
 
-	//start number is the second number in the final group (aka the number with the longest
-	//partition and highest pivot point). calculated from triangular number formula
-	long long start_number = (max_length + 1) * (max_length + 2) / 2;
-	long long current_factorial = factorial(max_length + 1); //used for faster calculation of factorial
-	long long pivot_stop = max_length - 1 - (goal - start_number) - 1; //when pivot reaches this location the maximum possible length decreases by 1
-	long long double_pivot_start = max_length;
-	if (max_length + start_number > goal) double_pivot_start--;
+	//note, this above line is the same thing as typing: int* combined_a1[3] = {&a1_1[0], &a1_2[0], &a1_3[0]},
+    //passing an array as an argument to a function is the same thing as passing a reference to the first element of
+    //the array.
 
-	for (long long pp = max_length - 1; pp >= 0; pp--) //pp stands for pivot point you pervs
+    //finally you'd need to create an array holding the sizes of these individual arrays to help prevent weird out of 
+    //index things from happening. This is because when an array is passed to a function via pointer, the function doesn't
+    //know how large the array actually is. Of course this isn't necessary, and with small and few enough arrays you can 
+    //avoid errors just by remembering the array lengths and making for loops that are the right size, but things can
+    //get complicated quickly.
+	int a1_array_lengths[3] = { 2, 4, 3 };
+
+	//Create an instance of the struct with all of these variables, struct is outlined above
+	arrayOfArrays a1(3, a1_array_lengths, combined_a1);
+
+	//Now the array containing an unknown number of arrays, all with different lenghts has been created (a1).
+	//Let's say you wanted to print the value of all the arrays contained in here in order, you could do something
+	//like shown here
+	for (int i = 0; i < a1.numberOfArrays; i++)
 	{
-		//reduce partition length when pivot point reaches appropriate spot
-		if (pp == pivot_stop) current_num -= final_value;
-
-		//separate multiplication and division into two steps to avoid floating point errors
-		current_num *= (pp + 3);
-		current_num /= (pp + 2);
-
-		//add new values
-		current_factorial /= (pp + 2);
-
-		//calculating the factorial each iteration is slow, keep a running tally of the current factorial instead
-		long long new_values = current_factorial * (pp + 3) * (pp + 1);
-		current_num += new_values;
-
-		//set final row value
-		if (pp == (max_length - 1)) final_value = current_num; //set the first time
-		else
+		for (int j = 0; j < *(a1.arrayLengths + i); j++)
 		{
-			final_value *= (pp + 3);
-			final_value /= (pp + 2);
+			//not sure how familiar you are with this, but the *() nomenclature is called de-referencing a pointer.
+			//by taking the pointer to the arrayLengths array and adding 'i' to it we get the individual pointers store in
+			//the arrayLengths array. We then dereference these pointers to get the actual length
+			std::cout << *(*(a1.overallArray + i) + j) << ", ";
 		}
-
-		//calculate no pivot value by multiplying case where pivot is at the very end
-		answer += new_values * (pp + 2) / (pp + 3);
-
-		//calculate double pivot by cascading every number up by 1. This value should only be
-		//calculated in the first iteration of loop if start_number + max_length < goal which is
-		//why there's the double_pivot_start variable
-		if (pp < double_pivot_start) answer += new_values * (pp + 2) / 2 * (pp + 4) / (pp + 3);
-
-		answer += current_num;
+		std::cout << std::endl;
 	}
-	//ran in 0.245 seconds (this is current run time when goal is set to 10e14)
+
+	//the above loop outputs the following because I got lazy with the commas:
+
+	/*
+	1, 99,
+    1, 2, 3, 4,
+    1, 39, 50,
+	*/
+
+	//in all honesty what I know about arrays and pointers may just be lacking and there could be a much much
+	//easier way to do this but for now this is all I got. Things start getting pretty complicated when you need
+	//to de-reference a pointer, which in turn yields a pointer that also needs to be de-reference (i.e.
+	//this line in the print loop: std::cout << *(*(a1.overallArray + i) + j) << ", "; but it's definitely great
+	//practice!
 
 	return { std::to_string(answer), std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - run_time).count() / 1000000000.0 };
 
--- main.cpp
+++ main.cpp
@@ -8,12 +8,12 @@ int main()
 	//All project euler questions are handled in separate header files. Each question is it's own functino which returns a pair where first is the answer and second is the time to run
 	//The question number is stored in the question_number variable automatically for ease of reading
 
-	//auto answer = test();
+	auto answer = test();
 	//auto answer = q94();
-	Run_1to50();
+	//Run_1to50();
 
-	//std::cout << "The answer is " << answer.first << std::endl;
-	//std::cout << "Program ran in " << answer.second << " seconds" << std::endl;
+	std::cout << "The answer is " << answer.first << std::endl;
+	std::cout << "Program ran in " << answer.second << " seconds" << std::endl;
 
 	//Run_51to100();
 
