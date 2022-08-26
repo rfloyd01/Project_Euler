a5ed922f7de90629292a4a9c07bda604e980e6aa
rfloyd01 <lynrdskynrd05@gmail.com>
2022-08-26 09:54:16 -0400

    resolved q29, 800x speedup

--- Questions/Q1-Q50/q29.h
+++ Questions/Q1-Q50/q29.h
@@ -2,35 +2,60 @@
 
 #include <Header_Files/pch.h>
 #include <Header_Files/functions.h> //includes primes()
+#include <Header_Files/print.h>
 #include <vector>
 #include <map>
+#include <cmath>
 
 //Distinct powers	
 std::pair<std::string, double> q29()
 {
 	auto run_time = std::chrono::steady_clock::now();
 	int answer = 0;
-	std::map<std::vector<int>, bool> used;
-	std::vector<int> primes; for (int i = 0; i <= 100; i++) primes.push_back(0);
 
-	for (int a = 2; a <= 100; a++)
+	const int limit = 100; //maximum allowable exponent for problem
+	int baseTwoLimit = log(limit) / log(2); //represents highest exponent where 2^baseLimit <= limit
+
+	//Calculate the total number of numbers below or equal to the limit that can be 
+	//described as a different number raised to a power. For example, if the limit was 
+	//30 then these numbers would be (4, 8, 9, 16, 25, 27). When a number can be expressed
+	//as multiple powers (i.e. 16 could be 2^4 aor 4^2) we put it in the category with the 
+	//higher exponent.
+	std::vector<int> powerNumbers(baseTwoLimit + 1, 0);
+	powerNumbers[1] = limit - 1; //can't raise 1 to any power so remove it
+
+	for (int i = baseTwoLimit; i > 1; i--)
 	{
-		std::vector<int> prime_factors = PrimeFactors(a);
-		std::vector<int> primes; for (int i = 0; i <= 100; i++) primes.push_back(0);
-		for (int i = 0; i < prime_factors.size(); i++) primes[prime_factors[i]]++;
-		//std::cout << a << ", "; vprint(prime_factors);
-		for (int b = 2; b <= 100; b++)
-		{
-			for (int i = 0; i < prime_factors.size(); i++) primes[prime_factors[i]]++;
-			if (!used[primes]) used[primes] = 1;
-		}
+		powerNumbers[i] = pow(limit, 1.0 / i) - 1; //remove 1 as an option
+		
+		//remove any numbers of the current power that could be described by a different
+		//number of a higher power
+		for (int j = i + i; j <= baseTwoLimit; j += i) powerNumbers[i] -= powerNumbers[j];
+		powerNumbers[1] -= powerNumbers[i];
 	}
-	
-	answer = used.size();
+
+	for (int i = 1; i <= baseTwoLimit; i++)
+	{
+		//use a modified sieve to remove powers that overlap
+		int sieve[limit + 1] = { 0 };
+
+		for (int j = 1; j < limit + 1; j++) sieve[j] = j;
+
+		for (int j = i - 1; j > 0; j--)
+			for (int k = 0; k <= j * limit / i; k += j / gcd(j, i)) sieve[k] = 0;
+
+		int total = 0;
+		for (int j = 0; j < limit + 1; j++)
+			if (sieve[j]) total++;
+
+		answer += total * powerNumbers[i];
+	}
+	answer -= powerNumbers[1]; //remove all powers of 1 that have been raised to the first power only (i.e. 6^1)
+
 	return { std::to_string(answer), std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - run_time).count() / 1000000000.0 };
 
 	//the answer is 9183
-	//ran in 0.0082711 seconds
+	//ran in 0.0000175 seconds
 }
 
 //NOTES
@@ -38,3 +63,26 @@ std::pair<std::string, double> q29()
 //prime factorization of each number found. All primes from 2-100 will be included in each numbers vector,
 //i.e. 10 = [0, 0 1, 0, 1, 0, 0, 0.... 0] where the ones represent 2^1 and 5^1. 25 = [0, 0, 0, 0, 2, 0, 0...0] represents 5^2.
 //A map is used to store each of these prime factor vectors to make sure that no duplicates are kept track off.
+
+/*
+Original Euler Code
+int answer = 0;
+//std::map<std::vector<int>, bool> used;
+//std::vector<int> primes; for (int i = 0; i <= 100; i++) primes.push_back(0);
+
+//for (int a = 2; a <= 100; a++)
+//{
+//	std::vector<int> prime_factors = PrimeFactors(a);
+//	std::vector<int> primes; for (int i = 0; i <= 100; i++) primes.push_back(0);
+//	for (int i = 0; i < prime_factors.size(); i++) primes[prime_factors[i]]++;
+//	//std::cout << a << ", "; vprint(prime_factors);
+//	for (int b = 2; b <= 100; b++)
+//	{
+//		for (int i = 0; i < prime_factors.size(); i++) primes[prime_factors[i]]++;
+//		if (!used[primes]) used[primes] = 1;
+//	}
+//}
+//
+//answer = used.size();
+//ran in 0.0082711 seconds
+*/
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Header_Files/pch.h>
-#include <Questions/Q1-Q50/q28.h>
+#include <Questions/Q1-Q50/q29.h>
 #include <Header_Files/test.h>
 
 int main()
@@ -7,7 +7,7 @@ int main()
 	//All project euler questions are handled in separate header files. Each question is it's own functino which returns a pair where first is the answer and second is the time to run
 	//The question number is stored in the question_number variable automatically for ease of reading
 
-	auto answer = q28();
+	auto answer = q29();
 	//auto answer = test();
 
 	//auto answer = regexPractice();
