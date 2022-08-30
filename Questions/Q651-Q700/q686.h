a7070bb1827a1df83187414f34822d5d0e118ab4
rfloyd01 <lynrdskynrd05@gmail.com>
2022-08-30 08:27:32 -0400

    solved q686

--- Questions/Q651-Q700/q686.h
+++ Questions/Q651-Q700/q686.h
@@ -1,98 +1,123 @@
 #pragma once
 
 #include <Header_Files/pch.h>
-#include <Header_Files/functions.h>
+#include <Header_Files/int_64x.h>
 
-//Inverse Digit Sum
-long long s(long long n, long long mod)
-{
-	return ((n % 9 + 1) * ModPow((long long)10, n / 9, mod, 1)) % mod - 1;
-}
-
-long long S(long long n, long long mod)
-{
-	long long multiple9 = n / 9 * 9; //the first division gets floored so multiple 9 won't necessarily equal n
-	long long S_value = ((6 * ModPow((long long)10, multiple9 / 9, mod, 1)) % mod - (multiple9 % mod + 6 % mod) % mod) % mod;
-
-	for (int i = 1; i <= (n % 9); i++) S_value = (S_value + s(multiple9 + i, mod)) % mod;
-	return S_value;
-}
-
-std::pair<std::string, double> q684()
+//Powers of Two
+std::pair<std::string, double> q686()
 {
 	auto run_time = std::chrono::steady_clock::now();
+	long long answer = 0;
 
-	//answer starts at 2 because that's the sum of f1 and f2
-	long long answer = 0, fibbonaci[2] = { 0, 1 }, mod = 1000000007;
+	double diff = (log10(124) - log10(123)) / log10(2);
+	int k = 0, hitNumber = 0;
 
-	for (int i = 2; i <= 90; i++)
+	while (true)
 	{
-		long long temp = fibbonaci[1];
-		fibbonaci[1] += fibbonaci[0];
-		fibbonaci[0] = temp;
-
-		answer = (answer + S(fibbonaci[1], mod)) % mod;
+		double upper = ( k++ + log10(124)) / log10(2);
+		if ((long long)upper > (long long)(upper - diff))
+		{
+			if (++hitNumber == 678910)
+			{
+				answer = (long long)upper;
+				break;
+			}
+		}
 	}
 
 	return { std::to_string(answer), std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - run_time).count() / 1000000000.0 };
 
-	//the answer is 922058210
-	//ran in 0.0017131 seconds
+	//the answer is 193060223
+	//ran in 0.0929607 seconds
 }
 
 //NOTES
 /*
-My Project Euler numbers have stagnated recently so I've decided to go on a low hanging fruit hunt. This problem is only a 5% difficulty level, and involves
-the first 90 fibonacci numbers. After checking with the int_64x class, the 90th fibonacci number just fits into a long long type. No matter what the number
-is, the way to minimze the length of the digits that sum to it is by mazimizing the number of 9's we have. For example, with the number 20, we could sum to it
-with all 1's if we wanted, i.e. 11111111111111111111, but even better would be to compress the 1's 9 at a time into a 9, i.e. 1199. There are two left over 1's
-in this case so they should be compressed into a 2, as less digits will always yield a lower number, despite the physicaly digit being larger. So 1199 goes to 
-299 which is indeed the lowest number whose digits sum to 20. Following this theory, I think the minimal number whose digits sum to a different number will 
-always follow the formula:
+My guess here is that there's going to be some kind of pattern here where every x-th number or so will start with 123. My first order of busines will be to use the 
+int_64x class to try and get the first few values and see if anything stands out. Before looking for patterns beginning with '123' I started with '12', any number 
+that starts with '123' will be a subset of the '12' pattern so it seemed like a good place to start looking.
 
-digit 1 = N % 9
-next N/9 amount of digits = 9
+An interesting pattern does occur here. It seems that there will be a span with no hits, then there will be either three or four hits that are 10 apart from each other, another span 
+of no hits and then three or four more all 10 away from eachother. Doing a search to find hits under 2^1000 gets the following results:
+[7, 80, 90, 100, 110, 183, 193, 203, 276, 286, 296, 369, 379, 389, 399, 472, 482, 492, 565, 575, 585, 595, 668, 678, 688, 761, 771, 781, 854, 864, 874, 884, 957, 967, 977]
 
-If we did this for the first 20 numbers would get: [1, 2, 3, 4, 5, 6, 7, 8, 9, 19, 29, 39, 49, 59, 69, 79, 89, 99, 199, 299]. Adding all of these values together
-yields 1,074 and matches the problem statement, confirming my theory. So figuring out the appropriate lowest number is a simple task, the only difficult thing
-is figuring out the appropriate value after modulus dividing it by 1000000007. It's easy enough to figure out the smallest number whose digits sum to 10,000 
-is 199999999... (1111 9's), but how to I get the modulus of this large number? Well, since the number will always end with a bunch of 9's this means that it
-will always be of the form (N % 9 + 1) * 10^(N / 9) - 1. Looking back to our example of 20, it will have the form (20 % 9 + 1) * 10^(20/9) - 1 = 3 * 10^2 - 1 = 299.
-If we can break down our large number into an equation like this then taking the modulus should be simple. With all of this knowledge, our answer should just be
-((fib_i % 9 + 1) * ModPow(10, fib_i / 9, 1000000007)) % 1000000007 - 1.
+excluding 2^7 which is on its own, here's the pattern for number of consecutive hits that are off by 10 [4, 3, 3, 4, 3, 4, 3, 3, 4, 3]. And here's the length of the gap 
+between these groups of hits: [73, 73, 73, 73, 73, 73, 73, 73, 73, 73] well that's interesting. The first term in a group of hits always occurs 73 powers later. Interesting,
+so there's definitely something going on here. Now to take a look at hits that start with '123'. The hits are [90, 379, 575, 864]. This means that the power of two will start 
+with '123' when it's the second value in one of the groups of 4 for powers of 2 that start with '12'. Searching for hits under 5000 gives:
+[90, 379, 575, 864, 1060, 1545, 1741, 2030, 2226, 2515, 2711, 3000, 3196, 3681, 3877, 4166, 4362, 4651, 4847]. I want to see the distance between hits as I'm sure there will
+be a similar pattern that was seen in the numbers start with '12'. I've taken the hits out to 2^8000 this time.
+[90, 289, 196, 289, 196, 485, 196, 289, 196, 289, 196, 289, 196, 485, 196, 289, 196, 289, 196, 289, 196, 485, 196, 289, 196, 289, 196, 289, 196, 485]. This is exactly what 
+I'm looking for. This pattern is very repeatable, with the exception of the first set, it seems that we always have [485, 196, 289, 196, 289, 196, 289, 196] and the pattern 
+repeats itself every 8 terms so with this it should be fairly trivial to find out answer. This pattern starts on the 6th term with a value of 2^1060, the 45th term would then 
+be 4.875 patterns away (or in other words, 4 full patterns and then 7/8 of the fifth pattern). In this case it would be easier to go 5 full paterns away and then subtract the 
+last term which is 196. The sum of a full pattern is 2136 so the 45th term in the sequence should be 2^(1060 + 5*21336 = 11740). Hmm, that doesn't match the question prompt
+though, the 45th term should be 12,710 which means that the pattern maybe doesn't repeat out into infinty. Here's what the difference pattern looks like for hits 25 - 50:
 
-I didn't get the answer right at first, and couldn't figure out why. After re-reading the problem I saw the issue, at first I thought they were asking for the sum
-of small s, s(fi) but it turns out that they want the sum of big S, S(fi) for the 2nd through 90th Fibonacci numbers. This definitely adds a layer of complexity
-to the problem. This means that we're going to be calculating all values of s(n) for n = 1 through n = F90 which is a 19 digit number. Calculating this all one
-at a time isn't feasible so instead we'll need to calculate the sum over a given range each time. Although this adds a layer of complexity, the problem still isn't
-that difficult. Looking at our range for s(1) - s(20) we see that a pattern forms. Every 9 numbers that gets added will have the numbers (1-9) one place further 
-than the 9 previous numbers and a 9 in the ones place 9 times in a row. Basically you multiply the sume of 1-9 (45) by a repunit and it to 9*9 (81) multiplied by
-the repunit that's one less. In equation form this would look something like:
 
-S(n) where n % 9 == 0 --> 45 * ((10 ^ (n / 9) - 1) / 9) + 81 * 
-sum these consecutive values together it would give us our value of big S, S(n) for every 9th number -> [45, 576, 5967, 59958]. The equations for these S(n) values
-are
+[196, 289, 196, 289, 196, 485, 485, 196, 289, 196, 289, 196, 485, 485, 196, 289, 196, 289, 196, 485, 485, 196, 289, 196, 289, 196]
 
-45 * 1 + 81 * 0 = 45
-45 * 11 + 81 * (1) = 576
-45 * 111 + 81 * (10 + 2) = 5,967
-45 * 1111 + 81 * (100 + 20 + 3) = 59,958
+So it looks like there are two 485 terms next to eachother here at terms 30 and 31, then again at 37 and 38. This would make the value of the 37th and 45th terms 10089 and
+12710 respectively. This complicates things, if the sequence changes over time then I won't be able to just use a simple formula to get to the answer like I thought I would,
+although if the differences still only include the numbers 196, 289 and 485 (which is simply 196 + 289) then all isn't lost. After carying out the sequence for the first 150 
+hits (starting with the 6th hit) this is what the pattern looks:
 
-So in each iteration we multiply 45 by an n-digit repunit and add that to 81 times the sum of all repunits that are less than n digits. A little off screen math 
-and I found that the sum of consecutive repunits can be characterized by the following equation.
+1545
+485, 196, 289, 196, 289, 196, 289, 196,
+3681
+485, 196, 289, 196, 289, 196, 289, 196,
+5817
+485, 196, 289, 196, 289, 196, 289, 196,
+7953
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 196, 289, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 196, 289, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485, 196,
+485, 485, 485, 485,
+681, 485, 485, 485
 
-Sum Repunits 1 - N = (10^(N+1) - 9N - 10) / 81
+So it's definitely changing quite a bit, furthermore, the code I wrote to generate this hits does so by moving forward by the original pattern. If the pattern changes
+and I'm only moving forward by the original pattern it's possible that I'm skipping over certain values. To confirm this I want to iterate over every number in the area
+where I see the pattern changing above, however, it becomes painstakingly slow to check every single number. If I knew the pattern was going to change every set number of 
+terms and by a set amount it would be one thing, but that doesn't appear to be the case. Assuming that I haven't missed any terms then the pattern lasts for 24, 49, 36 and 30
+terms from what I can see, but then only lasts for 4 before changing again which takes me to the end of my data. So I'm a little stumped here, considering that this problem
+is only supposed to be 5% difficulty there must be something major that I'm missing so it may be time to approach this from a different angle. Maybe it has something to do 
+with the prime factorization of the exponents? That seems unlikely though because the pattern is additive and not multiplicative. Looking at the binary forms of the numbers
+probably wont help either, since they're all powers of 2 it'll just be a 1 followed by a bunch of zeros.
 
-Putting everything together and we get that the value for every 9th S(n) is:
-S(n) where n % 9 == 0 --> 5 * (10 ^ (n / 9) - 1) + 10^(n / 9) - 9(n / 9 - 1) - 10
-S(n) = 6 * 10^(n/9) - n - 6
+If I'm not able to solve this with a pattern, maybe I can come up with some kind of equation to help. We're looking for integer powers of 2 that will be less than 124 * 10 ^ x and 
+less than 123 * 10 ^ x for all values of x. Using logarithms we can see that when x = 10, 2^x = 1,240,000,000,000 ---> xlog(2) = log(124 * 10,000,000,000) ---> xlog(2) = log(124) + log(10^10)
+---> xlog(2) = log(124) + 10 ---> x = (log(124) + 10) / log(2) ---> x = 40.173 and (log(123) + 10) / log(2) = 40.162. There aren't any whole numbers that come between 40.173 and 40.162
+so we know that there is no power of 2 that will get a value of 123 followed by 10 zeros. We can extend this to any power of 10 to easily see if there are any powers of 2 that will
+be a whole number. Our first hit found with the brute force algorithm was 2^90. 2^90 = 10^27  so applying this to the other equations (log(124) + (27-2)) / log(2) = 90.002 and
+(log(123) + (27-2)) / log(2) = 89.9907. In this scenario we see that the non-decimal part of the number extends from 89 to 90 which means that 2^90 will give a value that starts with 123.
 
-trying this formula out we get
-S(9) = 6 * 10^1 - 15 = 45
-S(18) = 6 * 10^2 - 24 = 576
-S(27) = 6 * 10^3 - 33 = 5,967
-S(36) = 6 * 10^4 - 42 = 59,958
+Basically what we get are two different linear equations:
+y1 = (log(123) + x) / log(2)
+y2 = (log(124) + x) / log(2)
 
-All of these values match so the formula appears to be correct. With this the problem should now be pretty easy. Divide each fibonocci number by 9 to get the value of
-S(floor(fib_i / 9)). Then calculate the values of little s, s(i) for i = floor(fib_i / 9) * 9 + 1 through fib_i which will be a maximum of only 8 calculations.
+y2 - y1 = ((log(124) + x) - (log(123) + x)) / log(2)
+y2 - y1 = (log(124) - log(123)) / log(2) = 0.01168
+
+So for any given power of 10, the value of the exponent that 2 is raised to to get the number 124(0) will always be greater than the exponent to get a number of the form 123(0)
+by a value of 0.01168. What this boils down to, is that as long as the decimal portion of the equation y = (log(124) + x) / log(2) is less than 0.01168, then we'll have a hit.
+
+I implemented a brute force-ish approach where I calculate all values of our upper range and then subtract the difference to the lower range (0.01168...) I then cast both of 
+these doubles to integers and if the upper integer is greater than the lower integer we have a hit. I didn't think this approach would be that effective but surprisingly it works
+in under 100 milliseconds. Just goes to show that what I consider to be brute force solutions aren't truly brute force these days.
 */
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <Header_Files/pch.h>
-#include <Questions/Q651-Q700/q684.h>
+#include <Questions/Q651-Q700/q686.h>
 #include <Header_Files/test.h>
 
 int main()
@@ -7,7 +7,7 @@ int main()
 	//All project euler questions are handled in separate header files. Each question is it's own functino which returns a pair where first is the answer and second is the time to run
 	//The question number is stored in the question_number variable automatically for ease of reading
 
-	auto answer = q684();
+	auto answer = q686();
 	//auto answer = test();
 
 	//auto answer = regexPractice();
